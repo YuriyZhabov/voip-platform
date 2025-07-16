f23895b2f62536bec5a5c89e3967b80403a87538
VoIP Platform <voip@platform.com>
2025-07-16 16:22:14 +0100

    Исправлена проблема с подключением FreePBX к Asterisk
    - Исправлено несоответствие паролей AMI между manager.conf и amportal.conf
    - Запущены все необходимые сервисы FreePBX (FastAGI, UCP Node Server)
    - Настроены правильные права доступа
    - FreePBX теперь полностью функционален и доступен по https://pbx.stellaragents.ru/admin
    - Устранена ошибка 'Cannot connect to Asterisk'

--- configs/apache/init-apache.sh
+++ configs/apache/init-apache.sh
@@ -7,6 +7,10 @@ a2enmod remoteip headers rewrite
 # Включаем наши конфигурации
 a2enconf freepbx freepbx-proxy
 
+# Отключаем стандартный сайт и включаем наш
+a2dissite 000-default
+a2ensite freepbx
+
 # Перезагружаем Apache
 service apache2 reload
 
